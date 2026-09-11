962e3564c4d31d364ce2b86be2c3503d4ae791d5
tria <tashubajpai160@gmail.com>
2026-09-11 19:54:16 +0530

    Update insightRoutes.js

--- backend/src/routes/insightRoutes.js
+++ backend/src/routes/insightRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/auth.js'
 import { getInsights } from '../controllers/insightController.js'
-
 const router = express.Router()
 
 router.use(authMiddleware)
